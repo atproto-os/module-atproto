8a64dfaeafb343339ba0157f8d7ad1d8bc71eb54
dxlliv <hello@dxlliv.dev>
2025-05-18 15:54:47 +0200

    feat: Add default atproto config

--- module.ts
+++ module.ts
@@ -32,6 +32,42 @@ export default defineNuxtModule({
 
     // retrieve owd owner profile from atproto
 
+    if (!_nuxt.options.atproto) {
+      _nuxt.options.atproto = {
+        serviceEndpoint: {
+          private: 'https://bsky.social',
+          public: 'https://public.api.bsky.app'
+        },
+        oauth: {
+          clientMetadata: {
+            remote: '',
+            local: {
+              client_id: 'https://nuxt-atproto.pages.dev/client-metadata.json',
+              client_name: 'nuxt-atproto',
+              client_uri: 'https://nuxt-atproto.pages.dev',
+              logo_uri: 'https://nuxt-atproto.pages.dev/logo.png',
+              tos_uri: 'https://nuxt-atproto.pages.dev',
+              policy_uri: 'https://nuxt-atproto.pages.dev',
+              redirect_uris: ['https://nuxt-atproto.pages.dev'],
+              scope: "atproto transition:generic",
+              grant_types: ["authorization_code", "refresh_token"],
+              response_types: ["code"],
+              token_endpoint_auth_method: 'none',
+              application_type: 'web',
+              dpop_bound_access_tokens: true
+            }
+          },
+          signInOptions: {
+            state: '',
+            prompt: 'login',
+            scope: 'atproto',
+            ui_locales: 'en',
+          },
+        },
+        debug: true,
+      }
+    }
+
     options.owner.profile = await getAtprotoDesktopOwner(
       new AtpAgent({
         service: _nuxt.options.atproto.serviceEndpoint.public,
